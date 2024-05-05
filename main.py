f2c75a0aee17f3e808d4e185c6d6f8a31961e14c
AlexEfimov <alex@efimov.mobi>
2024-05-05 17:11:39 +0300

    add bug fixed semi-final solid

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ PLAYER_IMAGE = 'IMAGES/Cartoon_ant_01.png'
 ENEMY_IMAGE = 'IMAGES/Spider_01.png'
 PRIZE_IMAGE = 'IMAGES/strawberry-01.png'
 HEADER_IMAGE = 'IMAGES/wood.png'
+HEADER_FNT_COLOR = (70, 30, 10)
 BG_IMAGE = 'IMAGES/grass.png'
 DOOR_CLOSED = 'IMAGES/door_closed.png'
 DOOR_OPENED = 'IMAGES/door_opened_01.png'
@@ -41,20 +42,16 @@ class GameObject(ABC):
         """ Отрисовка объекта на экране """
         screen.blit(self.image, self.rect)
 
-    def update(self):
-        """ Обновление состояния объекта """
-        pass
-
 
 class Player(GameObject):
     def __init__(self, image_path, position, size):
         super().__init__(image_path, position, size)
-        self.stamina = 100  # Начальная стамина
+        self.health = 100  # Начальная стамина
 
-    def decrease_stamina(self, amount):
-        self.stamina -= amount
-        if self.stamina <= 0:
-            self.stamina = 0
+    def decrease_health(self, amount):
+        self.health -= amount
+        if self.health <= 0:
+            self.health = 0
             print("Вы проиграли!")
             return True
         return False  # Флаг состояния окончания игры
@@ -64,7 +61,7 @@ class Player(GameObject):
 
         if 0 <= new_x <= SCREEN_WIDTH - PLAYER_SIZE[0]:
             self.rect.x = new_x
-        if EXTRA_HEIGHT <= new_y <= SCREEN_HEIGHT - PLAYER_SIZE[1]:
+        if EXTRA_HEIGHT <= new_y <= TOTAL_SCREEN_HEIGHT - PLAYER_SIZE[1]:
             self.rect.y = new_y
 
     def draw(self, screen):
@@ -107,7 +104,6 @@ class Door(GameObject):
             self.image = self.closed_image
             self.is_open = False
 
-
     def draw(self, screen):
         super().draw(screen)
 
@@ -124,10 +120,10 @@ class GameScreen(ABC):
         except Exception as e:
             self.background = pygame.Surface(self.size)
 
-
     @abstractmethod
     def draw(self, screen):
         screen.blit(self.background, (0, 0))
+
 
 class Playground(GameScreen):
     def __init__(self, width, height, bg_image, font_name, font_size, font_color, position):
@@ -136,27 +132,28 @@ class Playground(GameScreen):
     def draw(self, screen):
         screen.blit(self.background, self.position)
 
+
 class Header(GameScreen):
-    def __init__(self, width, height, bg_image, font_name, font_size, font_color, position, score, stamina):
+    def __init__(self, width, height, bg_image, font_name, font_size, font_color, position, score, health):
         super().__init__(width, height, bg_image, font_name, font_size, font_color, position)
-        self.stamina = stamina
+        self.health = health
         self.score = score
         self.font_color = font_color  # (70, 30, 10)
 
     def draw(self, screen):
         super().draw(screen)
-        stamina_text = self.font.render(f'STAMINA: {self.stamina}', True, self.font_color)
+        health_text = self.font.render(f'HEALTH: {self.health}', True, self.font_color)
         score_text = self.font.render(f'SCORE: {self.score}', True, self.font_color)
 
         # Отображение стамины и очков в верхней панели
-        screen.blit(stamina_text, (10, 10))  # Размещение немного ниже верхней границы
+        screen.blit(health_text, (10, 10))  # Размещение немного ниже верхней границы
         screen.blit(score_text, (600, 10))
 
-    def change_stamina(self, new_stamina):
-        self.stamina = new_stamina
+    def change_health(self, new_health):
+        self.health = new_health
 
-    def change_score(self, new_score):
-        self.score = new_score
+    def change_score(self, score):
+        self.score = score
 
 
 class LostScreen(GameScreen):
@@ -165,8 +162,6 @@ class LostScreen(GameScreen):
         self.score = score
         self.text = self.font.render(f'GAME OVER', True, (255, 255, 255))
         self.text_rect = self.text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 50))
-        self.score_text = self.font.render(f'SCORE: {self.score}', True, (70, 30, 10))
-        self.score_text_rect = self.score_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 10))
         self.control_text = self.font.render(f'[Пробел] - продолжить, [ESC] - выйти', True, (255, 255, 255))
         self.control_text_rect = self.control_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 10))
 
@@ -174,8 +169,14 @@ class LostScreen(GameScreen):
     def draw(self, screen):
         screen.fill((255, 0, 0))
         screen.blit(self.text, self.text_rect)
+        self.score_text = self.font.render(f'SCORE: {self.score}', True, (0, 75, 75))
+        self.score_text_rect = self.score_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 10))
         screen.blit(self.score_text, self.score_text_rect)
         screen.blit(self.control_text, self.control_text_rect)
+
+    def update_score(self, new_score):
+        self.score = new_score
+
 
 class WinScreen(GameScreen):
     def __init__(self, width, height, bg_image, font_name, font_size, font_color, position, score):
@@ -183,18 +184,20 @@ class WinScreen(GameScreen):
         self.score = score
         self.text = self.font.render(f'Поздравляю! Вы прошли уровень!', True, (255, 255, 255))
         self.text_rect = self.text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 50))
-        self.score_text = self.font.render(f'Счёт: {self.score}', True, (70, 30, 10))
-        self.score_text_rect = self.score_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 10))
         self.control_text = self.font.render(f'[Пробел] - продолжить, [ESC] - выйти', True, (255, 255, 255))
         self.control_text_rect = self.control_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 10))
-
 
     def draw(self, screen):
         super().draw(screen)
         screen.fill((50, 100, 50))
         screen.blit(self.text, self.text_rect)
+        self.score_text = self.font.render(f'Счёт: {self.score}', True, (250, 250, 0))
+        self.score_text_rect = self.score_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 10))
         screen.blit(self.score_text, self.score_text_rect)
         screen.blit(self.control_text, self.control_text_rect)
+    def update_score(self, new_score):
+        self.score = new_score
+
 
 class SoundManager:
     def __init__(self):
@@ -219,16 +222,17 @@ class Game:
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, TOTAL_SCREEN_HEIGHT))
         self.game_screen = Playground(SCREEN_WIDTH, SCREEN_HEIGHT, BG_IMAGE, FONT_NAME, FONT_SIZE, FONT_COLOR, (0, EXTRA_HEIGHT))
-        self.header = Header(SCREEN_WIDTH, EXTRA_HEIGHT, HEADER_IMAGE, FONT_NAME, FONT_SIZE, FONT_COLOR, (0, 0), 0, 0)
+        self.header = Header(SCREEN_WIDTH, EXTRA_HEIGHT, HEADER_IMAGE, FONT_NAME, FONT_SIZE, HEADER_FNT_COLOR, (0, 0), 0, 0)
         self.door = Door(DOOR_CLOSED, (SCREEN_WIDTH - DOOR_SIZE[0], EXTRA_HEIGHT), DOOR_SIZE, DOOR_OPENED)
-        self.player = Player(PLAYER_IMAGE, (0, SCREEN_HEIGHT - PLAYER_SIZE[1]), PLAYER_SIZE)
-        self.score = 0
-        self.lost_screen = LostScreen(SCREEN_WIDTH, SCREEN_HEIGHT, 'None', FONT_NAME, FONT_SIZE, FONT_COLOR, (0, 0), self.score)
-        self.win_screen = WinScreen(SCREEN_WIDTH, SCREEN_HEIGHT, 'None', FONT_NAME, FONT_SIZE, FONT_COLOR, (0, 0), self.score)
+        self.player = Player(PLAYER_IMAGE, (0, SCREEN_HEIGHT - PLAYER_SIZE[1] + EXTRA_HEIGHT), PLAYER_SIZE)
+
+        self.lost_screen = LostScreen(SCREEN_WIDTH, SCREEN_HEIGHT, 'None', FONT_NAME, FONT_SIZE, FONT_COLOR, (0, 0), 0)
+        self.win_screen = WinScreen(SCREEN_WIDTH, SCREEN_HEIGHT, 'None', FONT_NAME, FONT_SIZE, FONT_COLOR, (0, 0), 0)
         self.sound_manager = SoundManager()
         self.enemies = []
         self.prizes = []
         self.prize_count = 0
+        self.score = 0
         self.game_over = False  # Флаг состояния окончания игры
         self.victory = False    # Флаг победы в игре
 
@@ -237,15 +241,16 @@ class Game:
         self.prizes.clear()
         self.prize_count = 0
         self.score = 0
-        self.player.stamina = 100
+        self.player.health = 100
         self.game_over = False
         self.victory = False
-        self.player.rect.topleft = (0, SCREEN_HEIGHT - PLAYER_SIZE[1])
+        self.player.rect.topleft = (0, TOTAL_SCREEN_HEIGHT - PLAYER_SIZE[1])
         self.door.close()
 
     def run(self):
         # Воспроизведение фоновой музыки
         self.sound_manager.play_background_music()
+        # Начало игрового цикла
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -284,7 +289,7 @@ class Game:
                     if self.player.rect.colliderect(enemy.rect):
                         self.enemies.remove(enemy)                      # Удаляем противника
                         self.sound_manager.play_sound('collision')      # Воспроизведение звука при столкновении
-                        status = self.player.decrease_stamina(INJURY)   # Уменьшение стамины и проверка на проигрыш
+                        status = self.player.decrease_health(INJURY)    # Уменьшение стамины и проверка на проигрыш
                         if status:
                             self.game_over = True  # Поднятие флага окончания игры
                             self.victory = False   # Пометить, что игрок проиграл
@@ -298,18 +303,23 @@ class Game:
                         self.score += SCORE_PER_PRIZE             # Увеличение счета
                         if self.prize_count >= TOTAL_PRIZES and not self.door.is_open:
                             self.door.open()  # Открываем дверь, если собрано минимальное кол-во призов
-
+                            self.sound_manager.play_sound('door_open')
                 # Изменение стамины и очков в верхней панели
                 self.header.change_score(self.score)
-                self.header.change_stamina(self.player.stamina)
+                self.header.change_health(self.player.health)
                 self.header.draw(self.screen)
+
+
                 if self.prize_count >= TOTAL_PRIZES and self.player.rect.colliderect(self.door.rect):
                     self.game_over = True
                     self.victory = True
             else:
                 if not self.victory:
+                    self.lost_screen.update_score(self.score)
                     self.lost_screen.draw(self.screen)
                 else:
+                    print(f"Updated score before to win_screen: {self.score}")  # Для отладки
+                    self.win_screen.update_score(self.score)
                     self.win_screen.draw(self.screen)
 
                 keys = pygame.key.get_pressed()
